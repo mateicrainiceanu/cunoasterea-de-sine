759bbe47aa133e2cd9b664180101808d78550f34
mateicrainiceanu <100783062+mateicrainiceanu@users.noreply.github.com>
2023-07-08 13:53:04 +0300

    new bg-img

--- src/pages/HomeComponents/Landing.tsx
+++ src/pages/HomeComponents/Landing.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Landing() {
-  return (
-    <section id="acasa">
-    <div
-    className="carousel-item active image-bg"
-    style={{backgroundImage: "url(assets/img/landing.jpg)"}}
-    >
-      <div className="carousel-container">
-        <div className="container">
-          <h2 className="">Cunoașterea de sine.</h2>
-          <p className="">
-            <a href="#dece" className="de-ce">#DECE?</a> este important să te cunoști pe tine? <br />
-            Ajungi cea mai bună versiune a ta!
-          </p>
-          <a href="#scurta-descriere" className="btn-get-started">Află mai multe</a>
-        </div>
-      </div>
-    </div>
-  </section>
-  )
+	return (
+		<section id="acasa">
+			<div
+				className="carousel-item active image-bg"
+				style={{
+					backgroundImage: "url(https://www.definebusinessterms.com/wp-content/uploads/2021/11/autoconocimiento.jpg)",
+				}}>
+				<div className="carousel-container">
+					<div className="container">
+						<h2 className="">Cunoașterea de sine.</h2>
+						<p className="">
+							<a href="#dece" className="de-ce">
+								#DECE?
+							</a>{" "}
+							este important să te cunoști pe tine? <br />
+							Ajungi cea mai bună versiune a ta!
+						</p>
+						<a href="#scurta-descriere" className="btn-get-started">
+							Află mai multe
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default Landing;
